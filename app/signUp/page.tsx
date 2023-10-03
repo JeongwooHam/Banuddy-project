cb5690f700444cc22ad59d9f3e84e2fd3a8df91b
nasilKiM <nasikun@naver.com>
2023-10-03 22:10:00 +0900

    :sparkles: Add : json-server 회원가입 기능 1차

--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -1,7 +1,51 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { SyntheticEvent } from 'react'
+
 export default function SignUp() {
+  const router = useRouter()
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault()
+    const target = e.target as typeof e.target & {
+      email: { value: string }
+      password: { value: string }
+    }
+
+    const { email, password } = target
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ title: email.value, body: password.value }),
+    }
+
+    fetch(process.env.NEXT_PUBLIC_API_URL + 'users', options)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result)
+        router.refresh()
+        router.push(`/login`)
+      })
+  }
+
   return (
     <>
-      <h1>SignUp 페이지입니다</h1>
+      <h1>회원가입 페이지입니다</h1>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <input type="email" name="email" placeholder="이메일" />
+        </p>
+        <p>
+          <input type="password" name="password" placeholder="비밀번호" />
+        </p>
+        <p>
+          <input type="submit" value="회원가입" />
+        </p>
+      </form>
     </>
   )
 }
