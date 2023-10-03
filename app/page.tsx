57a3b32f86fdcb8278cd1a535c5460c2880ca368
jeongwooHam <hamjw0122@gmail.com>
2023-10-03 16:45:59 +0900

    ✨ shadcn UI 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+
 export default function Home() {
   return (
     <>
       <h2>Welcome to Banuddy!</h2>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Accordion UI입니다.</AccordionTrigger>
+          <AccordionContent>Accordion UI를 적용해봤습니다:D</AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </>
   )
 }
