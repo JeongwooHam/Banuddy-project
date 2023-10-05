f3fffaafdba13c372a97679801b884841373eb49
nasilKiM <nasikun@naver.com>
2023-10-05 20:51:06 +0900

    :lipstick: Add : 메인페이지 작업중

---
+++ app/main/_components/CatCarousel.tsx
@@ -0,0 +1,36 @@
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+const CatCarousel = () => {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 1500,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  }
+
+  return (
+    <>
+      <div className="w-full">
+        <Slider {...sliderSettings}>
+          <div>
+            <img src="/images/banner.png" alt="banuddy introduction" />
+          </div>
+          <div>
+            <img src="/images/banner.png" alt="banuddy introduction" />
+          </div>
+          <div>
+            <img src="/images/banner.png" alt="banuddy introduction" />
+          </div>
+        </Slider>
+      </div>
+    </>
+  )
+}
+
+export default CatCarousel
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,7 +1,56 @@
-export default function Main() {
+'use client'
+
+import React from 'react'
+import BannerAd from './_components/Banner'
+import DogCarousel from './_components/DogCarousel'
+import CatCarousel from './_components/CatCarousel'
+import './style.css'
+
+const Main: React.FC = () => {
   return (
     <>
-      <h1>main 페이지입니다</h1>
+      <div className="container">
+        {/* Navbar */}
+        <nav className="bg-gray-200 p-4">
+          <div className="flex justify-between items-center">nav bar</div>
+        </nav>
+
+        <div className="w-full">
+          <BannerAd />
+        </div>
+
+        {/* Pet Intro Section */}
+        <div className="w-full flex flex-col justify-center items-center">
+          <div className="w-full ">
+            <DogCarousel />
+            <CatCarousel />
+          </div>
+        </div>
+
+        {/* Button Section */}
+        <div className="flex w-1/2 text-center justify-center items-center mt-8">
+          <div className="flex flex-col ">
+            <h1>반려동물 입양</h1>
+            <p>
+              많은 유기동물들이 가족과 친구들을 기다리고 있습니다. 아이들에게
+              따뜻한 보금자리가 되어주세요!
+            </p>
+            <button className="submit-btn">입양 동물 소개</button>
+            <button className="submit-btn">전국 입양 공고</button>
+          </div>
+          <div className="flex flex-col ">
+            <h1>보호소 봉사</h1>
+            <p>
+              열악한 환경에 놓인 유기 동물들에게 도움의 손길이 절실합니다.
+              보호소 봉사 활동에 참여하여 사랑과 온기를 전해주세요!
+            </p>
+            <button className="submit-btn">보호소 확인하기</button>
+            <button className="submit-btn">봉사신청서 작성</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
+
+export default Main
