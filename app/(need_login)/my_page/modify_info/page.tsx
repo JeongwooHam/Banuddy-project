9f13019f661c704d61123d81a51b60773e53b4e2
jeongwooHam <hamjw0122@gmail.com>
2023-10-07 03:04:57 +0900

    ✨ feat: nav bar 라우팅

---
+++ app/(need_login)/my_page/modify_info/page.tsx
@@ -0,0 +1,3 @@
+export default function modifyInfo() {
+  return <h1>개인정보수정 페이지입니다</h1>
+}
--- app/(need_login)/my_page/page.tsx
+++
@@ -1,9 +0,0 @@
-import MyPageLayout from './layout'
-
-export default function myPage() {
-  return (
-    <MyPageLayout asideContent={<h2>Aside Content</h2>}>
-      <h1>myPage 페이지입니다</h1>
-    </MyPageLayout>
-  )
-}
