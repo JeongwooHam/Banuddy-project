800cdb811d7494df20ccc9ba524249d87c1bb0b3
jeongwooHam <hamjw0122@gmail.com>
2023-10-08 17:15:27 +0900

    ✨ Feat: api core 생성 및 전체 api response 타입 정의

--- app/api/adopt/seoul/seoul.api.ts
+++ app/api/adopt/seoul/seoul.api.ts
@@ -1,27 +1,12 @@
+import { GET } from '../../@core'
 import type { responseList, responsePhoto } from './seoul.type'
 
-export const getSeoulAnimalList = async (
-  start_index: number,
-  end_index: number,
-): Promise<responseList> => {
-  const results = await fetch(
+export const getSeoulAnimalList = (start_index: number, end_index: number) =>
+  GET<responseList>(
     `${process.env.NEXT_PUBLIC_SEOUL_URL}/${process.env.NEXT_PUBLIC_SEOUL_LIST_KEY}/json/TbAdpWaitAnimalView/${start_index}/${end_index}/`,
-    { next: { revalidate: 3600 } },
   )
 
-  const animalList: responseList = await results.json()
-  return animalList
-}
-
-export const getSeoulAnimalImages = async (
-  start_index: number,
-  end_index: number,
-): Promise<responsePhoto> => {
-  const results = await fetch(
+export const getSeoulAnimalImages = (start_index: number, end_index: number) =>
+  GET<responsePhoto>(
     `${process.env.NEXT_PUBLIC_SEOUL_URL}/${process.env.NEXT_PUBLIC_SEOUL_PHOTO_KEY}/json/TbAdpWaitAnimalPhotoView/${start_index}/${end_index}/`,
-    { cache: 'no-store' },
   )
-
-  const animalPhoto: responsePhoto = await results.json()
-  return animalPhoto
-}
