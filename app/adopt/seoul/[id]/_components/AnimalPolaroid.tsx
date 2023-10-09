d162f07752593b8905dc495aee392b2429c2d240
jeongwooHam <hamjw0122@gmail.com>
2023-10-09 16:14:34 +0900

    ✨ Feat: 상세 정보 및 이미지 연결

--- app/adopt/seoul/[id]/_components/AnimalPolaroid.tsx
+++ app/adopt/seoul/[id]/_components/AnimalPolaroid.tsx
@@ -5,29 +5,28 @@ import { useAtom } from 'jotai'
 import { animalAtom } from '../../_components/OneSeoulAnimal'
 import { mergedItem } from '../../_components/SeoulAnimalList'
 import State from '@/constants/adoptState'
+import { colorMatch } from '@/constants/colorMatch'
 
 const AnimalPolaroid: React.FC = () => {
   const [targetAnimal] = useAtom(animalAtom)
 
   if (targetAnimal) {
     const target = targetAnimal as mergedItem
+    const { SPCS, NM, ADP_STTUS } = target.list
+    const dynamicClassName = `absolute top-[80px] left-[90px] w-[75px] h-[75px] bg-primary-300 rounded-full p-[10px]`
     return (
       <div className="w-[500px] border-solid border-[1px] border-gray-100 shadow relative">
         <ImageSlide />
-        <div className="absolute top-[80px] left-[90px] w-[75px] h-[75px] bg-primary-300 rounded-full p-[10px]">
-          <p className="w-[45px] ml-[5px] text-center">
-            {State[target.list.ADP_STTUS]}
-          </p>
+        <div className={dynamicClassName}>
+          <p className="w-[45px] ml-[5px] text-center">{State[ADP_STTUS]}</p>
         </div>
-        <div className="flex mt-[30px]">
+        <div className="flex mt-[140px]">
           <span>
             <Image src={Cat} alt="cat" />
           </span>
           <div className="text-[24px] ml-[30px] text-center">
             안녕하세요 저는{' '}
-            <span className="text-[40px] font-bold">
-              {target.list.NM.split('(')[0]}
-            </span>
+            <span className="text-[40px] font-bold">{NM.split('(')[0]}</span>
             입니다.
           </div>
         </div>
--- app/adopt/seoul/[id]/_components/ImageSlide.tsx
+++ app/adopt/seoul/[id]/_components/ImageSlide.tsx
@@ -6,6 +6,9 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import SampleImage from '/public/images/sampleImage.png'
 import Image from 'next/image'
+import { useAtom } from 'jotai'
+import { animalAtom } from '../../_components/OneSeoulAnimal'
+import { mergedItem } from '../../_components/SeoulAnimalList'
 
 function ImageSlide() {
   const settings = {
@@ -17,16 +20,32 @@ function ImageSlide() {
     // dotsClass: "custom-dots" > 시간 남으면 슬라이드 커스텀
   }
 
-  return (
-    <div className="w-[350px] m-center mt-[60px]">
-      <Slider {...settings}>
+  const [targetAnimal] = useAtom(animalAtom)
+
+  if (targetAnimal) {
+    const target = targetAnimal as mergedItem
+    const { photo } = target
+
+    return (
+      <div className="w-[350px] m-center mt-[60px]">
+        <Slider {...settings}>
+          {/* <Image src={SampleImage} alt="sample" />
         <Image src={SampleImage} alt="sample" />
         <Image src={SampleImage} alt="sample" />
-        <Image src={SampleImage} alt="sample" />
-        <Image src={SampleImage} alt="sample" />
-      </Slider>
-    </div>
-  )
+        <Image src={SampleImage} alt="sample" /> */}
+          {photo?.map((image) => (
+            <Image
+              src={`https://${image.PHOTO_URL}`}
+              alt="sample"
+              width={300}
+              height={150}
+              key={image.PHOTO_NO}
+            />
+          ))}
+        </Slider>
+      </div>
+    )
+  }
 }
 
 export default ImageSlide
