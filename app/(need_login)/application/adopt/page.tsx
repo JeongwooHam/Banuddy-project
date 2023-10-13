0be94f23415b7a6edf1c05aa498b14ee11490e94
jeongwooHam <hamjw0122@gmail.com>
2023-10-13 17:19:10 +0900

    🔥 Remove: 중복 파일 삭제

--- app/(need_login)/application/adopt/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdoptApplication() {
-  return (
-    <>
-      <h1>입양 신청 페이지입니다.</h1>
-    </>
-  )
-}
--- app/(need_login)/application/volunteer/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function VolunteerApplication() {
-  return (
-    <>
-      <h1>봉사 신청 페이지입니다.</h1>
-    </>
-  )
-}
