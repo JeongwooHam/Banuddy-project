43d93651995e9585788b58f657d6839281cf701a
JeongwooHam <hamjw0122@gmail.com>
2023-11-01 19:01:35 +0900

    🎉 chore: test commit

--- app/adopt/national/[id]/page.tsx
+++ app/adopt/national/[id]/page.tsx
@@ -1,18 +1,5 @@
-import { useRouter } from 'next/navigation'
-import AnimalDetail from './_components/AnimalDetail'
-import AnimalPolaroid from './_components/AnimalPolaroid'
-import AnimalInfoText from './_components/AnimalInfoText'
-
 const Detail: React.FC = () => {
-  return (
-    <div>
-      <div className="flex justify-between w-content m-center mb-[100px]">
-        <AnimalPolaroid name="뚜비" />
-        <AnimalInfoText />
-      </div>
-      <AnimalDetail type="dog" />
-    </div>
-  )
+  return <div>전국 동물 공고 상세 페이지</div>
 }
 
 export default Detail
